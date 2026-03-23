ddacc123e8afd812483835f0abdaf7aaa56e8f0f
SganzerlaGui <gui.sganzerla2007@gmail.com>
2026-03-23 12:05:24 -0300

    Estudos para Check Point

--- Desenv.py
+++ Desenv.py
@@ -1,9 +1,61 @@
-import random
+#import random
 
-entrada = input("Digite 3 numeros separados por espaço: ")
+#entrada = input("Digite 3 numeros separados por espaço: ")
 
-numeros = entrada.split()
+#numeros = entrada.split()
 
-print("Os números digitados foram: ", numeros)
+#print("Os números digitados foram: ", numeros)
 
-print("Obrigado por usar o programa!")
+#print("Obrigado por usar o programa!")
+
+
+#Estudando para o Check Poinnt-------------------------------------
+
+#temperatura = int(input("Qual a temperatura aatual do motor do seu carro? "))
+#
+#if temperatura < 70:
+#    print(f"O seu motor está frio!")
+#
+#elif temperatura >= 70 and temperatura <= 90:
+#    print(f"A temperatura do seu carro está ideal! Fique traanquilo!")
+#
+#else:
+#    print(f"A temperatura do seu carro está muito alta! Proccure ajuda imediatamente")
+
+#numero = int(input("Digite um nummero: "))
+
+#if numero / 2 >= 0 and numero > 0:
+#    print(f"O  numero {numero} é par e positivo!")
+#elif numero / 2 != 0 and numero > 0:
+#    print(f"O numero {numero} é impar e positivo!")
+#elif numero / 2 <= 0 and numero < 0:
+#    print(f"O numero {numero} é par e negativo!")
+#elif numero / 2 != 0 and numero < 0:
+#    print(f"O numero {numero} é impar e negativo!")
+#else:
+#    print(f"O numero {numero} é zero!")
+
+#metros = float(input("Qual a distancia em metros? "))
+#preco = float(input("Qual o valor do metro? "))
+#valor = metros * preco 
+#pagar = valor
+#if metros >= 100:
+#    valor = valor - valor * 0.10 
+#    print(f"O valor total foi de R$ {float(pagar):.2f}, com o desconto você vai pagar apenas R$ {float(valor):.2f} ")
+#else: 
+#    print(f"O valor a paagar é de R$ {float(pagar):.2f}")
+
+#resistencia = float(input("Qual a resistência do solo? "))
+#if resistencia < 10:
+#    print(f"Solo fragil - requer estacas")
+#elif resistencia >= 10 and resistencia <= 20:
+#    print(f"Solo está firme - sapata comum")
+#else:
+#    print(f"Solo rochoso - perfuração Especial")
+
+#login = str(input("Seu login aqui >>" ))
+#senha = str(input("Sua senha aqui >>" ))
+#if login == "admin" and senha == "fiap123":
+#    print(f"Bem vindo {str(login)}!")
+#else:
+#    print("Login ou senha incorretos!")
